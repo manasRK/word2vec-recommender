74a1e1f3f36d482272623b8e630a0fec94d7de6d
Manas Ranjan Kar <manasrkar.nmims@gmail.com>
2016-06-22 22:37:03 +0530

    Updated with file processing + training

--- semsim_train.py
+++ semsim_train.py
@@ -6,7 +6,54 @@ import re
 stop = stopwords.words('english')
 import gensim
 import string
+import redis
+import simplejson
+import gzip
 from unidecode import unidecode
+
+from phrases_extractor import get_phrases
+
+import logging
+import argparse
+
+price_obj = redis.Redis("localhost", port=6379, db=1)
+
+logger = logging.getLogger(__name__)
+
+
+def parse(filename):
+  g = gzip.open(filename,"r")
+  for l in g:
+    yield json.dumps(eval(l))
+
+
+def getRange(price):
+
+  price  = float(price)
+  if price <= 10:
+    return 10
+  elif 10 < price <= 100:
+    return 100
+  elif 100 < price <= 250:
+    return 250
+  elif 250 < price <= 500:
+    return 500
+  elif 500 < price <= 1000:
+    return 1000
+  elif 1000 < price <= 2000:
+    return 2000
+  elif 2000 < price <= 5000:
+    return 5000
+  elif price > 5000:
+    return 9999
+
+
+def process_phrases(text):
+  phrases = get_phrases(text)
+
+  for phrase in phrases:
+    text = text.replace(phrase, phrase.replace(' ', '_'))
+  return text
 
 
 def pre_process(m):
@@ -20,27 +67,72 @@ def pre_process(m):
     #print m
     return m
 
+
 #iterator on Amazon Reviews  
 class MySentences(object):
     def __init__(self, dirname):
         self.dirname = dirname
- 
-    def __iter__(self):
-        count = 0
-        for fname in os.listdir(self.dirname):
-            for line in open(os.path.join(self.dirname, fname)):
-                count +=1
-                line = pre_process(line)
-                print count
-                yield line.split()
-                
-#directory for training
-inputfile="output/" # output directory
-#model save name
-fname="output_models/model_22062016.txt"
 
-sentences = MySentences(inputfile)
-model = gensim.models.Word2Vec(sentences, min_count=10, workers=8, negative=10, sg=1, size = 300, sample=1e-5, hs=1) #v6 
-model.init_sims(replace=True)
-model.save(fname)
+    def __iter__(self):
+        print os.listdir(self.dirname)
+        for fname in os.listdir(self.dirname):
+            logger.info('Processing file %s', fname)
+            count=0
+            l=0
+            for e in parse((os.path.join(self.dirname, fname))):
+                review= json.dumps(e.get("reviewText"))
+                title = json.dumps(e.get("summary"))
+                all_text = title + ". " + review
+                all_text = process_phrases(all_text)
+              
+                if len(review.split())>10: # don't consider reviews less than 10 words
+                  productId = json.dumps(e.get("asin"))
+                  try:
+                    price = getRange(price_obj.get(productId))
+                  except:
+                    price = 0
+                  
+                  final_data = "<" + str(price) + "> <" + category + "> <" + productId + "> " + all_text + " <" + str(price) + "> <" + category + "> <" + productId + ">"
+                  
+                  sents = pre_process(final_data)
+                  count +=1
+                  l+=len(sents)
+                  print 'Count: '," ",count," ",l," ",sents
+                  yield sents.split()
+
+                
+def train(data_folder, model_path):
+    sentences = MySentences(data_folder)
+    model = gensim.models.Word2Vec(sentences, min_count=10, workers=8, negative=10, sg=1, size = 300, sample=1e-5, hs=1) #v6 
+    model.init_sims(replace=True)
+    model.save(model_path)
+
+if __name__ == "__main__":
+    logging.root.setLevel(level=logging.INFO)
+    logger.info("running %s", ' '.join(sys.argv))
+
+    # check and process cmdline input
+    program = os.path.basename(sys.argv[0])
+    if len(sys.argv) < 2:
+        print(globals()['__doc__'] % locals())
+        sys.exit(1)
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument(
+        "-d", "--data", required=True,
+        help="Input data folder.")
+    parser.add_argument(
+        "-m", "--model_output", required=True,
+        help="Output model file, in word2vec format")
+    args = parser.parse_args()
+    
+    data_folder, model_file = (args.data, args.model_output)
+    logger.info('Picking data from %s...', data_folder)
+    
+    train(data_folder, model_path)
+    
+    logger.info("Saving model file %s in %s", model_file, os.path.abspath(model_file))
+    logger.info("Finished running %s", program)
+    
 

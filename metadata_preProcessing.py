5dab8efc9a479c3f725a0099ec57813bfd8b246e
Manas Ranjan Kar <manasrkar.nmims@gmail.com>
2016-06-23 14:32:38 +0530

    added try excepts

--- metadata_preProcessing.py
+++ metadata_preProcessing.py
@@ -7,22 +7,35 @@ data_obj = redis.Redis("localhost", port=6379, db=2)
 
 def preProcess(row):
 	print row
-	global final_dic
 	temp = []
+	
 	productId = row["asin"]
+	
 	try:
 		boughtTogether = row["related"]["bought_together"]
 		for ele in boughtTogether:
 			temp.append("b_t_"+ele)
+	except:
+		pass
+	
+	try:
 		alsoBought = row["related"]["also_bought"]
 		for ele in alsoBought:
 			temp.append("a_b_"+ele)
+	except:
+		pass
+	
+	try:
 		alsoViewed = row["related"]["also_viewed"]
 		for ele in alsoViewed:
 			temp.append("a_v_"+ele)
-		data_obj.set(productId, temp)
 	except:
 		pass
+	
+	if temp ==[]:
+		pass
+	else:
+		data_obj.set(productId, temp)
 	
 	
 

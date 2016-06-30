d9a79d54c641f0502c2564c1a9d1b858e4908433
akhilgupta <akhilgupta.official@gmail.com>
2016-06-30 13:00:26 +0530

    loading data from redis  :bar_chart:

--- loadRedis.py
+++ loadRedis.py
@@ -7,20 +7,21 @@
 import redis
 from bottle import route, run
 
+
 redis_obj = redis.Redis(host = 'localhost', port = 6379, db =3)
-@route('/search/redis/<pids>')
-def search(pids):
-	pids = eval(pids)
+@route('/search/redis/<input_dic>')
+def search(input_dic):
+	input_dic = eval(input_dic)
 	global redis_obj
 	res_dic = {}
 #	pids = ['B005MYAMS4','B00534M24A','B0043M666I','B000GHG9VG','0299128741']
-	for id in pids:
+	for ids in input_dic["pids"]:
 		try:
-			res = redis_obj.get(id)
+			res = redis_obj.get(ids)
 			if len(eval(res))==4:
-				res_dic.update({id:res})
+				res_dic.update({ids:res})
 		except:
 			pass
 	return res_dic
-run(host = '0.0.0.0', port = 8003, debug = True) 
+run(host = '0.0.0.0', port = 8002, debug = True) 
 	

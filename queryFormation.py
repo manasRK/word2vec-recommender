8dcb5ce86298cf8012ff5cca358f406f9034614e
akhilgupta <akhilgupta.official@gmail.com>
2016-06-30 12:57:33 +0530

    query to token formation

---
+++ queryFormation.py
@@ -0,0 +1,26 @@
+import re
+
+q1 = "michael jackson, jazz, price 50"
+q2 = "michael jackson"
+q3 = "pid B123"
+
+def queryTokenize(query):
+	
+	tokens = {}
+	tokens["items"] = []  #for one or more item in query e.g. q1
+	for ele in query.split(","):
+		ele = ele.strip()
+		if re.search('\d+',ele):
+			temp = ele.split()
+			if temp[0] == "pid":
+				tokens.update({"pid":temp[1]})
+			if temp[0] == "price":
+				tokens.update({"price":temp[1]})
+		else:
+		
+			tokens["items"].append(ele.replace(" ","_"))
+			
+
+	return tokens
+
+print queryTokenize(q2)

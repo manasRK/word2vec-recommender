586a954628f00cfb6c8624145aec16410ac68644
codeorbit <akhilgupta.official@gmail.com>
2016-07-01 18:21:48 +0000

    added ranking and price filter

--- loadRedis.py
+++ loadRedis.py
@@ -4,24 +4,31 @@
 # @Email: akhilgupta.official@gmail.com 
 # @Github Username: codeorbit
 
+import ast
 import redis
-from bottle import route, run
-
-
 redis_obj = redis.Redis(host = 'localhost', port = 6379, db =3)
-@route('/search/redis/<input_dic>')
-def search(input_dic):
-	input_dic = eval(input_dic)
+def redisSearch(input_dic):
+#	input_dic = eval(input_dic)
 	global redis_obj
 	res_dic = {}
 #	pids = ['B005MYAMS4','B00534M24A','B0043M666I','B000GHG9VG','0299128741']
+	res_count = 0
+	try:
+		price = float(input_dic["price"]) 
+	except:
+		price = 0.0
+	print "price : ",price
 	for ids in input_dic["pids"]:
-		try:
-			res = redis_obj.get(ids)
-			if len(eval(res))==4:
-				res_dic.update({ids:res})
-		except:
-			pass
+		if res_count<=15:
+			try:
+				res = redis_obj.get(ids)
+				res = eval(res)
+#				print "price from redis : ", float(res[0])
+				if len(res)==2 and float(res[0])>price:
+					res.insert(0,ids)
+					res.insert(2,"music")
+					res_dic.update({ids:res})
+					res_count = res_count+1		
+			except:
+				pass
 	return res_dic
-run(host = '0.0.0.0', port = 8002, debug = True) 
-	

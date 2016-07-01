78177f71c275a5e463ee17acf552060912e2ed03
codeorbit <akhilgupta.official@gmail.com>
2016-07-01 18:23:02 +0000

    added ranking and IR

--- loadReviewModel.py
+++ loadReviewModel.py
@@ -14,11 +14,10 @@ import queryFormation as QF
 import re
 import urllib2
 import helpers
-
+import ast
+import requests
 redis_obj = redis.Redis(host = 'localhost', port = 6379, db = 3)
-model = gensim.models.Word2Vec.load('modelPath')
-
-
+model = gensim.models.Word2Vec.load('output_models/music_v4.txt')
 
 @route('/search/review/<query>')
 def search(query):
@@ -30,16 +29,28 @@ def search(query):
 	for key, val in tokens.items():
 		if key == "items":
 			for item in val:
+				print "item : ", item
 				res = model.most_similar(positive = [item], topn = 1000)
+				res_pid = res_pid + helpers.filterPid(str(res))
+		if key == "orig_pid":
+			try:
+				val = "<"+str(val.lower())+">"
+				print val
+#				print type(val)
+				res = model.most_similar(positive = [val],topn = 1000)
+#				print "res :: ",res 
 				res_pid = res_pid + helpers.filterPid(res)
-		if key == "pid":
-			res = model,most_similar(positive = [val],topn = 1000)
-				res_pid = res_pid + helpers.filterPid(res)
-
-
-	meta_module_input["pids"] = res_pid
-	meta_module_input["price"] = tokens["price"] 
-	result_meta_module = urllib2.urlopen('api call to meta model').read()
-	result_meta_module = eval(result_meta_module)
-	return result_meta_module
-run(host = '0.0.0.0', port = 8000, debug = True) 
+				
+			except Exception as e:
+				print "Exception from review model : ",e
+				pass
+				
+	print "final result length : ", len(res_pid)
+	meta_module_input = {}
+	meta_module_input = tokens
+	meta_module_input["pids"] = res_pid[:10]
+	print "meta module ",meta_module_input 
+	result_meta_module = requests.get('''http://173.255.113.135:8000/search/meta/'''+str(meta_module_input))
+	print type(eval(result_meta_module.text))	
+	return eval(result_meta_module.text)
+run(host = '0.0.0.0', port = 8080 , debug = True)

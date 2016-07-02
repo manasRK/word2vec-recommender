d34d82c6f96777c44a1cfad8aac0f77207aacd6e
akhilgupta <akhilgupta.official@gmail.com>
2016-07-02 12:13:33 +0530

    regex to get the pid from review model

--- helpers.py
+++ helpers.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Akhil Gupta
+# @Email:  akhilgupta.official@gmail.com  
+# @Github username: @codeorbit
+# @Last Modified by:   Akhil Gupta
+# @Last Modified time: 2016-07-1
+
 import re
 def filterPid(res):
 	res_pid = []  

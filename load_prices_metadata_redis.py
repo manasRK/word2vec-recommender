6a3fe6c7a30eb64e4bdd22cea703880f5534f7f8
akhilgupta <akhilgupta.official@gmail.com>
2016-07-02 12:14:16 +0530

    added comment

--- load_prices_metadata_redis.py
+++ load_prices_metadata_redis.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# @Author: Akhil Gupta
+# @Email:  akhilgupta.official@gmail.com  
+# @Github username: @codeorbit
+# @Last Modified by:   Akhil Gupta
+# @Last Modified time: 2016-07-1
 
 import json
 import gzip
